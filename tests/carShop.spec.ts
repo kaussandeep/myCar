b8161895e0c0b8627f0cc27f2b89631976a0b971
Sandeep Kaushik <sandeep.kaushik@qlik.com>
2023-02-11 14:25:40 +0100

    chore: stabilize test

--- tests/carShop.spec.ts
+++ tests/carShop.spec.ts
@@ -8,7 +8,9 @@ test.beforeEach(async ({ page }) => {
 
 test("Car Configurator flow: ", async ({ page }) => {
   await page.getByRole("button", { name: "Menu" }).click();
+  await page.getByRole("button", { name: "Buy" }).isVisible();
   await page.getByRole("button", { name: "Buy" }).click();
+  await page.getByRole("link", { name: "Car Configurator" }).isVisible();
   await page.getByRole("link", { name: "Car Configurator" }).click();
   const configuratorLandingPageHeading = await page
     .getByRole("heading", { name: "Design your Volvo" })
